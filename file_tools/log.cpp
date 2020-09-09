2acaba6f54152602ae3c151552d6fcb7dd96288d
QilimiWxu <qq1053620912@outlook.com>
2020-09-09 17:47:51 +0800

    修改CMakeLists.txt的错误 修改log判断文件类型和写入文件错误

--- file_tools/log.cpp
+++ file_tools/log.cpp
@@ -24,9 +24,20 @@ namespace qilimi{
 
         char path[256] = { 0 };
 
-        strcpy(path, file);
+        int index = 0;
+        const char* _pstr = file;
+        while(*_pstr != '\0' && *_pstr != '.' ){
+            _pstr++;
+            index++;
+        }
 
-        strcpy(path + strlen(path), "_back");
+        memcpy(path, file, index);
+
+        strcpy(path + index, "_back");
+
+        strcpy(path + strlen(path), file + index);
+
+        printf("new file: %s. old file:%s. \n", path, file);
 
         //删除
         remove(path);
@@ -46,25 +57,26 @@ namespace qilimi{
     int xn_write_log_debug(const char* log){
         std::lock_guard<std::mutex> lock(g_debug_mutex);
         //获取文件名称
-        const char* file = g_debug_path.size() >= 3 ? g_debug_path_default : g_debug_path.c_str();
+        const char* file = g_debug_path.size() <= 3 ? g_debug_path_default : g_debug_path.c_str();
         struct stat buffer;
-        //读取我呢间信息
+        //读取文件信息
         int ret = stat(file, &buffer);
-        if(ret != 0){
-            return -1;//文件不存在或者是其他错误
-        }
-        //判断文件爱你类型
-        if(buffer.st_mode != S_IFREG){
-            //不是是文件
-            return -2;
-        }
-        //判断文件大小
-        if(buffer.st_size > file_size_max){
-            deal_too_big_file(file);
+        if(ret == 0){
+            //判断文件类型
+            if(buffer.st_mode & S_IFREG == S_IFREG){
+                printf("file type:%d. %d.\n", S_IFREG, buffer.st_mode);
+                //不是是文件
+                return -2;
+            }
+            //判断文件大小
+            if(buffer.st_size > file_size_max){
+                deal_too_big_file(file);
+            }
         }
         //写日志
-        FILE *fp = fopen(file, "wb");
+        FILE *fp = fopen(file, "a+");
         if(fp == NULL){
+            printf("open faile:%s \n", file);
             return -1;
         }
         fseek(fp, 0, SEEK_END);
@@ -75,25 +87,26 @@ namespace qilimi{
     int xn_write_log_error(const char* log){
         std::lock_guard<std::mutex> lock(g_error_mutex);
         //获取文件名称
-        const char* file = g_error_path.size() >= 3 ? g_error_path_default : g_error_path.c_str();
+        const char* file = g_error_path.size() <= 3 ? g_error_path_default : g_error_path.c_str();
         struct stat buffer;
         //读取我呢间信息
         int ret = stat(file, &buffer);
-        if(ret != 0){
-            return -1;//文件不存在或者是其他错误
-        }
-        //判断文件爱你类型
-        if(buffer.st_mode != S_IFREG){
-            //不是是文件
-            return -2;
-        }
-        //判断文件大小
-        if(buffer.st_size > file_size_max){
-            deal_too_big_file(file);
+        if(ret == 0){
+            //判断文件爱你类型
+            if(buffer.st_mode & S_IFREG == S_IFREG){
+                printf("file type:%x. %x.\n", S_IFREG, buffer.st_mode);
+                //不是是文件
+                return -2;
+            }
+            //判断文件大小
+            if(buffer.st_size > file_size_max){
+                deal_too_big_file(file);
+            }
         }
         //写日志
-        FILE *fp = fopen(file, "wb");
+        FILE *fp = fopen(file, "a+");
         if(fp == NULL){
+            printf("open faile:%s \n", file);
             return -1;
         }
         fseek(fp, 0, SEEK_END);
